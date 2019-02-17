691023e923d5a80721e4d97dd41569fb25ff07aa
hbchevelle68 <aportante10@gmail.com>
2019-02-17 13:02:40 -0800

    created errors.py || implemented basic testing and exception handling

--- shamebot/overwatch/temp.py
+++ shamebot/overwatch/temp.py
@@ -1,12 +1,36 @@
-from bs4 import BeautifulSoup
+#std && 3rd party
+from bs4 import BeautifulSoup as BS
 import requests
-from overwatch import consts
 
+#internal
+from overwatch import consts as owc
+from overwatch import errors
 #https://playoverwatch.com/en-us/career/pc/HBchevelle68-11717
 
 class Overwatch:
 
-    bnet_name = ""
+    bnet_name = None
+    hero = None
+    hero_val = 0
 
-    def __init__(self, acct_full, hero=None):
-        pass
+    def __init__(self, acct_full=None, hero=None):
+        try:
+            if acct_full is None:
+                raise errors.EmptyAccountString
+            else:
+                self.bnet_name = str(acct_full)
+
+            if hero is None:
+                self.hero = "ALL HEROES"
+            elif hero not in owc.char_options:
+                raise errors.InvalidHeroName
+            else:
+                self.hero = str(hero)
+                self.hero_val = owc.char_options[hero]
+
+        except errors.InvalidHeroName:
+            print("Hero name invlaid -- Use proper case (ex: Ana, Roadhog, etc)")
+            print()
+        except errors.EmptyAccountString:
+            print("Account string cannot be empty")
+            print()
